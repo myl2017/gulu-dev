2143c510e7451688cb130cf5c20aac3e21c521f0
孟艳利 <706695013@qq.com>
2020-01-05 20:47:22 +0800

    使用递归算法 找出被选中的item

--- src/cascader.vue
+++ src/cascader.vue
@@ -4,7 +4,8 @@
             {{result}}<span>&nbsp;</span>
         </div>
         <div class="popover-wrapper" v-if="popoverVisible">
-            <cascader-items :items="source" class="popover" :height="popoverHeight" :selected="selected" @update:selected="onUpdateSelected"></cascader-items>
+            <cascader-items :items="source" class="popover" :height="popoverHeight" :selected="selected"
+                            @update:selected="onUpdateSelected"></cascader-items>
         </div>
     </div>
 </template>
@@ -22,9 +23,12 @@
             },
             selected: {
                 type: Array,
-                default: ()=> {
+                default: () => {
                     return []
                 }
+            },
+            loadData: {
+                type: Function
             }
         },
         data() {
@@ -52,7 +56,50 @@
 //        },
         methods: {
             onUpdateSelected(newSelected) {
-                this.$emit('update:selected',newSelected)
+                this.$emit('update:selected', newSelected)
+                let lastItem = newSelected[newSelected.length - 1]
+                let simplest = (children, id) => {
+                    return children.filter(item => item.id === id)[0]
+                }
+                let complex = (children, id) => {
+                    let noChildren = []
+                    let hasChildren = []
+                    children.forEach(item => {
+                        if (item.children) {
+                            hasChildren.push(item)
+                        } else {
+                            noChildren.push(item)
+                        }
+                    })
+                    let found = simplest(noChildren, id)
+                    if (found) {
+                        return found
+                    } else {
+                        found = simplest(hasChildren, id)
+                        if (found) {
+                            return found
+                        }
+                        else {
+                            for (let i = 0; i < hasChildren.length; i++) {
+                                found = complex(hasChildren[i].children, id)
+                                if (found) {
+                                    return found
+                                }
+                            }
+                            return undefined
+                        }
+//                        return hasChildren.length > 0 ? complex(hasChildren, id) : undefined
+                    }
+                }
+                let updateSource = (result) => {
+                    let copy = JSON.parse(JSON.stringify(this.source))
+                    let toUpdate = complex(copy, lastItem.id)//this.source.filter(item => item.id === lastItem.id)[0]
+                    toUpdate.children = result
+                    this.$emit('update:source', copy)
+//                    this.$set(toUpdate, 'children', result)
+                }
+                this.loadData(lastItem, updateSource) //回调：把别人传给我的函数调用一下
+                // 调回调的时候传一个函数，这个函数理论应该被调用
             }
         },
         computed: {
@@ -67,26 +114,26 @@
 
     .cascader {
         position: relative;
-        .trigger {
-            height: $input-height;
-            display: inline-flex;
-            align-items: center;
-            padding: 0 1em;
-            min-width: 10em;
-            border: 1px solid $border-color;
-            border-radius: $border-radius;
-        }
-        .popover-wrapper {
-            position: absolute;
-            top: 100%;
-            left: 0;
-            background: white;
-            display: flex;
-            @extend .box-shadow;
-            margin-top: 8px;
-            .label {
-                white-space: nowrap;
-            }
-        }
+    .trigger {
+        height: $input-height;
+        display: inline-flex;
+        align-items: center;
+        padding: 0 1em;
+        min-width: 10em;
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+    }
+    .popover-wrapper {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background: white;
+        display: flex;
+    @extend .box-shadow;
+        margin-top: 8px;
+    .label {
+        white-space: nowrap;
+    }
+    }
     }
 </style>
--- src/demo.vue
+++ src/demo.vue
@@ -2,7 +2,7 @@
     <div class="demo" id="app">
         <div style="padding: 20px;">
             <!--<g-cascader :source="source" popover-height="200px" :selected="selected" @update:selected="selected = $event"></g-cascader>-->
-            <g-cascader :source="source" popover-height="200px" :selected.sync="selected"></g-cascader>
+            <g-cascader :source.sync="source" popover-height="200px" :selected.sync="selected" :loadData="loadData"></g-cascader>
 
         </div>
     </div>
@@ -12,7 +12,12 @@
     import db from './db'
 
     function ajax(parentId = 0) {
-        return db.filter((item) => item.parent_id == parentId)
+        return new Promise((success) => {
+            setTimeout(() => {
+                let result = db.filter((item) => item.parent_id == parentId)
+                success(result)
+            })
+        })
     }
 
     export default {
@@ -23,7 +28,26 @@
         data() {
             return {
                 selected: [],
-                source: ajax()
+                source: []
+            }
+        },
+        created() {
+            ajax(0).then(result => {
+                this.source = result
+            })
+        },
+        methods: {
+            loadData(item, updateSource) {
+                let id = item.id
+                ajax(id).then((result) => {
+                    updateSource(result) //回调：把别人传给我的函数调用一下
+                })
+            },
+            xxx () {
+                ajax(this.selected[0].id).then(result => {
+                    let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
+                    this.$set(lastLevelSelected, 'children', result)
+                })
             }
         }
     }
